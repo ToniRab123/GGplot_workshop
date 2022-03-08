d43f17293969aa60b90d0b2949c220ee5972b73a
Toni Rabasco <tonitonitone@Tonis-Air.home>
2022-03-07 21:21:09 -0500

    data cleaning

--- GGPlot_project.R
+++ GGPlot_project.R
@@ -1,34 +1,33 @@
-#Setting up the environment
-#check if tidyverse is installed 
-packages <-c("tidyverse", "skimr", "RColorBrewer")
+## title: "ggplot2::workshop(...)"
+
+## Set up the environment
+##### (check if tidyverse installed)
+
+packages <- c("tidyverse", "skimr", "RColorBrewer")
+install.packages(setdiff(packages, rownames(installed.packages())))
+
 library(tidyverse)
-#check for other 2 packages
 library(skimr)
 library(RColorBrewer)
-install.packages("RColorBrewer")
-#Data
+search()
+
+## 1. Data
+
 iris[1:20,]
-#Data Structure -tidyverse
-iris >%>
-  skimr::skim()
-iris >%>
-  skimr::skim()
-mean(sepal.length)
-#Data Structure (tidyverse) 
+
+### Data Structure (tidyverse)
+
+# iris %>% error
+  # skimr::skim() need to load dplyr package that defines function first
+install.packages("dplyr")
+library(dplyr)
 iris %>%
-  skimr::skim()
-#Data Cleaning
-####tidyverse 
-set.seed(100)
-iris_clean <- iris %>% 
-  mutate(Trial = sample(3, n(), replace = TRUE))
-set.seed(100)
-iris_clean <- iris %>% 
-  mutate(Trial = sample(3, n(), replace = TRUE))
-set.seed(100)
+  skimr::skim() 
+
+###tidyverse 
+####data cleaning 
+
+set.seed(100) #dealing with randome process, if you dont set a random seed you R code is not reproducible 
 iris_clean <- iris %>%
-  mutate(Trial = sample (3),n(), replace = TRUE) 
-find.package("magrittr")
-set.seed(100)
-iris_clean <- iris %>%
-  mutate(Trial = sample (3),n(), replace = TRUE) 
+   mutate(Trial = sample(3, n(), replace = TRUE)) #create new variable named Trial, nor n number of rows in data frame assign a # randomly selected for 1-3, true makes it use 1,2,3 
+iris_clean %>% skim()
